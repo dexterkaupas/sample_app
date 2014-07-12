0d3518920d16fbbc6666d438a14998bd3b76852d
Dexter <dexterkaupas@gmail.com>
2014-07-12 14:00:20 +0200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe User do
 
+
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
@@ -9,7 +10,14 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
@@ -31,4 +39,6 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+
 end
